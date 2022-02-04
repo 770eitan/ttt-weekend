db19c373a3c49b53d44c94ed01a4ff28542335b4
770eitan <carl158@gmail.com>
2022-02-04 12:16:24 -0600

    finish init

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
-
+// const WinningPaths = [[0,1,2][0,3,6][0,4,8][1,4,7][2,5,6][2,4,6][3,4,5][6,7,8]]
+// const WinningPathsSum = WinningPathsSum.reduce()
+// console.log(WinningPaths)
 /*---------------------------- Variables (state) ----------------------------*/
 // let squareX 'X'
 // let squareO  'O'
-// let reset 
+
 // let squareStatus 
-// let turnTracker   
-// let winner 
+let turnTracker= 0
+let winner = null 
+// if (WinningPaths === 3){
+// 	let winner = 1
+// } else if WinningPaths = -3 {
+// 	let winnner = -1 
+// } else WinningPaths === 2 || WinningPaths === -2 {
+// 	message.textContent("It's a Tie!")
+// }
+
 // let tie 
 // let stillPlaying
 
@@ -40,7 +48,10 @@ init()
 function init(){
 array.innerHTML = null
 message.textContent = "Welcome, X goes first!"
+turnTracker = 1
+winner = null
 }
+
 console.log(init)
 
 // 1) Define the required variables used to track the state of the game:
