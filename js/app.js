969c833ff0ef696ffca083f8a8d834d66cef3a6b
770eitan <carl158@gmail.com>
2022-02-04 10:26:26 -0600

    cache element references

--- js/app.js
+++ js/app.js
@@ -3,16 +3,56 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+// let squareX 'X'
+// let squareO  'O'
+// let reset 
+// let squareStatus 
+// let turnTracker   
+// let winner 
+// let tie 
+// let stillPlaying
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const board = document.querySelector('.board') 
+const message = document.getElementById('message')
+console.log(message)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
-
 /*-------------------------------- Functions --------------------------------*/
 
+/*-------------------------------- Functions --------------------------------*/
+//AAU I should be able to know whose turn it is
+//AAU I should be able to click a box and it turn to my sign
+//
+
+
+
+// 1) Define the required variables used to track the state of the game:
+  // None of these variables will need to hold a value when they are defined
+
+	// 1.1) Use an array to represent the squares on the board.    
+
+	// 1.2) Use a turn variable to track whose turn it is.
+
+	// 1.3) Use a winner variable to represent three different game states:
+	  // a player that won
+	  // a tie has occured
+	  // or a game that is still in play.
+
+
+// 2) Store cached element references on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant:
+
+// 3) Upon loading, the app should:
+// 	3.1) Call an initialize function to initialize the state variables
+// 	3.2) Render those values to the page
+// 	3.3) Wait for the user to click a square
+
+// 4) Define the required constants
+
+// 5) Handle a player clicking a square
+
+// 6) Handle a player clicking the replay button
+// ```
