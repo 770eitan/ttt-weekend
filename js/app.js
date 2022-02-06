50539d8348cc9f21790a167618e1029c6c0b3bc1
770eitan <carl158@gmail.com>
2022-02-06 10:57:42 -0600

    Finish display winner

--- js/app.js
+++ js/app.js
@@ -26,6 +26,8 @@ let board
 const gameBoard = document.querySelector('.board') 
 const squareEls = document.querySelectorAll(".square")
 let message = document.getElementById('message')
+const imageX = document.createElement ('imageX')
+imageX.src =
 /*----------------------------- Event Listeners -----------------------------*/
 
 
@@ -35,7 +37,7 @@ let message = document.getElementById('message')
 init()
 
 function init(){
-	board = [null,null,1,-1,null,null,null,null,null]
+	board = [null,null,1,-1,1,-1,1,1,1]
 	message.textContent = "Welcome, X goes first!"
 	turnTracker = 1
 	winner = null
@@ -46,71 +48,28 @@ function render (){
 	console.log(squareEls)
 	board.forEach(function (square, idx){
 		console.log(square, idx)
-		
+		//Display which square was chosen and whos turn it is
 		if (square === 1){ 
-			squareEls[idx].textContent= "Chicken"
-
+			squareEls[idx].textContent = "X"
+			message.textContent = "X's turn"
 		}
 		else if (square === -1){
-			squareEls[idx].textContent= "Burrito"
+			squareEls[idx].textContent= "Y"
+			message.textContent = "Y's turn"
 
 		}
-		else {
-			squareEls[idx].textContent= ""
+		//display Winner status
+		
+		if (winner !== null) {
+			if (winner === 3) {
+		message.textContent = "X wins"
+			} else if (winner === -3) {
+				message.textContent = "Y Wins"
+			} else if (board.forEach (function (!null){
+				message.textContent = "Tie"
+			}))
+
 		}
 	})
 }
 
-// // if (!isWinner){ `It is your turn ${turnTracker ? 1: -1} ` }
-
-// // function renderX (){
-// // 	let sqr=1
-// // 	boardArray.forEach(function (sqr, idx){
-// // 		const squareX = document.getElementsByClassName('.board');
-// // 		board.style.background = 'black';
-// // 		return squareX
-// // })
-
-// // }
-
-// // function renderO (){
-// // 	let sqr=-1
-// // 	boardArray.forEach(function (sqr, idx){
-// // 		const squareO = document.getElementsByClassName('.board');
-// // 		board.style.background = 'red';
-// // 		return squareO
-// // })
-// // }
-// console.log(renderX)
-
-
-
-
-
-
-// 1) Define the required variables used to track the state of the game:
-  // None of these variables will need to hold a value when they are defined
-
-	// 1.1) Use an array to represent the squares on the board.    
-
-	// 1.2) Use a turn variable to track whose turn it is.
-
-	// 1.3) Use a winner variable to represent three different game states:
-	  // a player that won
-	  // a tie has occured
-	  // or a game that is still in play.
-
-
-// 2) Store cached element references on the page that will be accessed in code more than once in variables to make code more concise, readable, and performant:
-
-// 3) Upon loading, the app should:
-// 	3.1) Call an initialize function to initialize the state variables
-// 	3.2) Render those values to the page
-// 	3.3) Wait for the user to click a square
-
-// 4) Define the required constants
-
-// 5) Handle a player clicking a square
-
-// 6) Handle a player clicking the replay button
-//
